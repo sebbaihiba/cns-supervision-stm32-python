9a5a50efc0196f2e65d6506f6f2d730d4188c569
Hiba SEBBAI <sebbaihiba@gmail.com>
2026-09-24 11:49:58 +0200

    Initialize database before loading history

--- python-app/show_history.py
+++ python-app/show_history.py
@@ -1,10 +1,10 @@
-from database import lire_historique
+from database import init_database, lire_historique
 
+init_database()
 historique = lire_historique()
 
 print("===== HISTORIQUE DES EVENEMENTS =====\n")
 
 for ligne in historique:
-    date_heure, temps_ms, systeme, defaut, etat = ligne
-
-    print(f"{date_heure} | {temps_ms} ms | {systeme} | {defaut} | {etat}")
+    date_heure, compteur, systeme, defaut, etat = ligne
+    print(f"{date_heure} | compteur={compteur} | {systeme} | {defaut} | {etat}")
